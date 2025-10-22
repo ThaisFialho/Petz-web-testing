b363fc539131e368003c6f2d22c624701f1b5735
Thais Fialho <thaisfialho301@gmail.com>
2025-10-22 19:32:16 -0300

    Adicionado mudanças nos arquivos main e page

--- Automation_Login/main.py
+++ Automation_Login/main.py
@@ -1,23 +1,19 @@
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 import data
 from page import PetzPage
 
 class TestPetz:
 
     def test_set_login(self):
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         self.driver.get(data.PETZ_URL)
         self.petz = PetzPage(self.driver)
         email = data.EMAIL
         password = data.PASSWORD
-        self.petz.set_login(email,password)
+        self.petz.set_login(email, password)
+        self.petz.set_enter()
         assert self.petz.get_email_field() == email
         assert self.petz.get_password_field() == password
         self.driver.quit()
-
-
-
-
-
-
--- Automation_Login/page.py
+++ Automation_Login/page.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class PetzPage:
@@ -16,20 +18,41 @@ class PetzPage:
         self.driver.find_element(*self.enter_button).click()
 
     def set_login(self,email,password):
-        self.driver.find_element(*self.email_field).send_keys(email)
-        self.driver.find_element(*self.password_field).send_keys(password)
-        self.set_enter()
+        wait = WebDriverWait(self.driver, 20)
 
+        # Fecha o banner de cookies, se aparecer
+        try:
+            cookies_btn = wait.until(
+                EC.element_to_be_clickable((By.ID, "onetrust-accept-btn-handler"))
+            )
+            cookies_btn.click()
+        except:
+            pass
+
+        # Espera o campo de e-mail ficar visível e interagível
+        email_input = wait.until(EC.visibility_of_element_located(self.email_field))
+        # Garante que nenhum overlay esteja bloqueando
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", email_input)
+        self.driver.execute_script("arguments[0].focus();", email_input)
+        email_input.clear()
+        email_input.send_keys(email)
+        password_input = wait.until(EC.visibility_of_element_located(self.password_field))
+        password_input.clear()
+        password_input.send_keys(password)
+
+
+        #self.driver.find_element(*self.email_field).send_keys(email)
+        #self.driver.find_element(*self.password_field).send_keys(password)
+        #self.set_enter()
 
 
     # Para assert
     def get_email_field(self):
-        #return self._wait_for(self.email_field).get_property('value')
-        return self.driver.find_element(*self.email_field).text
+        return self.driver.find_element(*self.email_field).get_attribute('value')
 
 
     def get_password_field(self):
-        return self.driver.find_element(*self.password_field).text
+        return self.driver.find_element(*self.password_field).get_attribute('value')
 
 
 
